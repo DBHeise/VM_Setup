df1716cd2a8da5fae1bee6b231fd4da9519e6960
David B. Heise <david@h2net.heiseink.com>
2017-08-14 19:55:11 -0400

    updates to get the run to work correctly

--- run.cmd
+++ run.cmd
@@ -1,9 +1,10 @@
 @echo off
+
 :checkPrivileges 
 NET FILE 1>NUL 2>NUL
-if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( powershell "Start-Process -FilePath %0 -Verb runas")
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( powershell "Start-Process -FilePath %0 -WorkingDirectory %~dp0 -Verb runas")
 exit /b 
 
 :gotPrivileges
 
-powershell -ExecutionPolicy ByPass -NoLogo -File %~dp0\run.ps1 -NoExit
+powershell -ExecutionPolicy ByPass -NoLogo -File %~dp0\run.ps1 -NoExit -WorkingDirectory %~dp0
--- run.ps1
+++ run.ps1
@@ -1,10 +1,14 @@
-$scriptsInOrder = Get-ChildItem ($pwd) -Directory | ForEach-Object { Get-ChildItem $_\*.ps1 -Recurse } | Where-Object {!$_.Name.StartsWith("~")} | Sort-Object
+[CmdletBinding()]
+$myPath = Split-Path $script:MyInvocation.MyCommand.Path
+$scriptsInOrder = Get-ChildItem ($myPath) -Directory | ForEach-Object { Get-ChildItem (($_.FullName) + "\*.ps1") -Recurse } | Where-Object {!$_.Name.StartsWith("~")} | Sort-Object
 
 
 $scriptsInOrder | % { 
     $shortname = $_.Name.Replace(".ps1","")
-    $longname = $_.FullName.Replace($pwd, "")
+    $longname = $_.FullName.Replace($myPath, "")
     $scriptBlock = Get-Content -Path $_ -Raw
+
+    Write-Verbose -Message ("Running file: " + $_.FullName)
 
     if ($shortname.StartsWith("_")) {
         $script = Get-Content -Path $_ -Raw
@@ -12,6 +16,8 @@ $scriptsInOrder | % {
             Invoke-Expression (gc $_ -Raw)
         }        
     } else {
-        & $_
+        & $_.FullName
     }
 }
+
+Start-Sleep -Seconds 10
