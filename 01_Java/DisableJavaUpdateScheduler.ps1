51dea04aab2e8b64ade2c8a9a471d095dc96a7c0
David B Heise <david@heiseink.com>
2017-08-15 09:59:18 -0400

    update to JAVA scripts

---
+++ 01_Java/DisableJavaUpdateScheduler.ps1
@@ -0,0 +1,17 @@
+
+Remove-Item -Path "HKLM\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Run" -Name "SunJavaUpdateSched" -Force
+Remove-Item -Path "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" -Name "SunJavaUpdateSched" -Force
+
+$jusched="C:\Program Files\Common Files\Java\Java Update\jusched.exe"
+
+if (Test-Path $jusched) {
+	Rename-Item -Path $jusched -NewName ($jusched + ".bak")
+}
+
+$jusched="C:\Program Files (86)\Common Files\Java\Java Update\jusched.exe"
+if (Test-Path $jusched) {
+	Rename-Item -Path $jusched -NewName ($jusched + ".bak")
+}
+
+
+
