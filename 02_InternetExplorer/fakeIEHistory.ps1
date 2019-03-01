d5d466bf6175bc05c10841a9c6c0ffc90dbe98a2
DBHeise <david@heiseink.com>
2019-03-01 11:13:22 -0500

    removing a module dependency

--- 02_InternetExplorer/fakeIEHistory.ps1
+++ 02_InternetExplorer/fakeIEHistory.ps1
@@ -1,4 +1,4 @@
-﻿if (Test-NetConnection) {
+﻿if (Test-Connection "internetbeacon.msedge.net" -Quiet) {
     1..100 | % {
         [System.Diagnostics.Process]::Start("http://www.randomwebsite.com/cgi-bin/random.pl")
     }
